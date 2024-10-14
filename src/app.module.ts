391ca1f566fcf6145ee3fb7901c11a38cd527c14
jimyungkoh <kojimyung1206@naver.com>
2024-10-14 20:48:49 +0900

    refactor: 공통 - 로거 - 모듈 export 및 import 방식 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ApplicationModule } from './application/application.module';
-import { LoggerModule } from './common/logger/logger.module';
+import { LoggerModule } from './common/logger';
 import { DomainModule } from './domain/domain.module';
 import { InfrastructureModule } from './infrastructure/infrastructure.module';
 import { PresentationModule } from './presentation/presentation.module';
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { WinstonLoggerService } from './common/logger/winston.logger.service';
+import { WinstonLoggerService } from './common/logger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
