dcd35ab79412f56f2e1d381e0514353ef438642b
jimyungkoh <kojimyung1206@naver.com>
2024-11-22 12:14:30 +0900

    feat: OutboxEvent 모델 추가
    - OutboxEvent 모델을 정의하는 outbox-event.model.ts 파일을 추가했습니다.
    - OutboxEventStatus 및 OutboxEventTypes를 정의하여 이벤트 상태와 유형을 관리합니다.
    - OutboxEventModel 클래스를 통해 OutboxEvent의 속성을 캡슐화하고, Prisma의 OutboxEvent를 기반으로 하는 변환 메서드를 구현했습니다.

---
+++ src/domain/models/outbox-event.model.ts
@@ -0,0 +1,51 @@
+import { OutboxEvent } from '@prisma/client';
+
+export const OutboxEventStatus = {
+  INIT: 'INIT',
+  SUCCESS: 'SUCCESS',
+  FAIL: 'FAIL',
+} as const;
+
+export type OutboxEventStatus =
+  (typeof OutboxEventStatus)[keyof typeof OutboxEventStatus];
+
+export const OutboxEventTypes = {
+  ORDER_CREATED: 'order.created',
+  ORDER_PAYMENT: 'order.payment',
+  ORDER_DEDUCT_STOCK: 'order.deduct_stock',
+  ORDER_SUCCESS: 'order.success',
+  ORDER_FAILED: 'order.failed',
+} as const;
+
+export type OutboxEventType =
+  (typeof OutboxEventTypes)[keyof typeof OutboxEventTypes];
+
+export type OutboxEventModelProps = {
+  aggregateId: string;
+  eventType: OutboxEventType;
+  payload: string;
+  status: OutboxEventStatus;
+};
+
+export class OutboxEventModel {
+  readonly aggregateId: string;
+  readonly eventType: string;
+  readonly payload: string;
+  readonly status: OutboxEventStatus;
+
+  constructor(props: OutboxEventModelProps) {
+    this.aggregateId = props.aggregateId;
+    this.eventType = props.eventType;
+    this.payload = props.payload;
+    this.status = props.status;
+  }
+
+  static from(props: OutboxEvent) {
+    return new OutboxEventModel({
+      aggregateId: props.aggregateId,
+      eventType: props.eventType as OutboxEventType,
+      payload: JSON.stringify(props.payload),
+      status: props.status,
+    });
+  }
+}
