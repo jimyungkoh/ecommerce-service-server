af2723dc78c173d2dae7d8636ceb3309ba0c1bee
jimyungkoh <kojimyung1206@naver.com>
2024-11-27 21:58:12 +0900

    feat: 애플리케이션 설정 및 구조 개선
    - 이벤트 에미터 설정 상세화
      - 이벤트 구분자 및 리스너 관련 옵션 설정
      - 메모리 누수 관리 옵션 추가
    - 카프카 마이크로서비스 연결 구성
      - 컨슈머 그룹 설정
      - 리밸런싱 및 토픽 자동 생성 옵션 추가
      - 메시지 처리 시작 위치 설정

--- src/presentation/consumer/product.event.consumer.ts
+++ src/presentation/consumer/product.event.consumer.ts
@@ -1,18 +1,37 @@
-import { Controller } from '@nestjs/common';
-import { EventPattern, Payload } from '@nestjs/microservices';
+import {
+  Controller,
+  Inject,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
+import { ClientKafka, EventPattern, Payload } from '@nestjs/microservices';
 import { ProductFacade } from 'src/application/facades';
-import { CreateOrderInfo } from 'src/domain/dtos';
 import { OutboxEventTypes } from 'src/domain/models/outbox-event.model';
+import { AppLogger, TransientLoggerServiceToken } from '../../common/logger';
+import { CreateOrderInfo } from '../../domain/dtos';
+import { KafkaClientKey } from '../../common/kafka/kafka.module';
 
 @Controller()
-export class ProductEventConsumer {
-  constructor(private readonly productFacade: ProductFacade) {}
+export class ProductEventConsumer implements OnModuleInit, OnModuleDestroy {
+  constructor(
+    @Inject(KafkaClientKey)
+    private readonly kafkaClient: ClientKafka,
+    private readonly productFacade: ProductFacade,
+    @Inject(TransientLoggerServiceToken)
+    private readonly logger: AppLogger,
+  ) {}
+
+  async onModuleDestroy() {
+    await this.kafkaClient.close();
+  }
+
+  async onModuleInit() {
+    await this.kafkaClient.connect();
+    this.kafkaClient.subscribeToResponseOf(OutboxEventTypes.ORDER_CREATED);
+  }
 
   @EventPattern(OutboxEventTypes.ORDER_CREATED)
-  handleOrderCreated(@Payload() data: CreateOrderInfo) {
-    return this.productFacade.processOrderDeductStock({
-      info: data,
-      aggregateId: `order_${data.order.id}`,
-    });
+  handleOrderCreated(@Payload() orderInfo: CreateOrderInfo) {
+    return this.productFacade.processOrderDeductStock(orderInfo);
   }
 }
