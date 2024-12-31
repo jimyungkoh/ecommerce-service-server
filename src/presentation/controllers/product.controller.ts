4fe7cdc0bb970438e33c98ba23eda7f879c27bfe
jimyungkoh <kojimyung1206@naver.com>
2024-12-31 17:03:35 +0900

    feat: ProductController에 로거 주입
    - ProductController에 AppLogger 주입하여 로깅 기능 추가
    - 생성자에서 TransientLoggerServiceToken을 사용하여 로거 인스턴스화

--- src/presentation/controllers/product.controller.ts
+++ src/presentation/controllers/product.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Inject, Param, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Effect, pipe } from 'effect';
 import { ProductFacade } from 'src/application/facades';
+import { AppLogger, TransientLoggerServiceToken } from 'src/common/logger';
 import { QueryDateRequestDto } from '../dtos/product/request';
 
 @ApiTags('/products')
 @Controller('/products')
 export class ProductController {
-  constructor(private readonly productUseCase: ProductFacade) {}
+  constructor(
+    private readonly productUseCase: ProductFacade,
+    @Inject(TransientLoggerServiceToken)
+    private readonly logger: AppLogger,
+  ) {}
 
   @Get('/:productId')
   getProducts(@Param('productId') productId: number) {
